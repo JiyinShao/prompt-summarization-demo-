724311117d027530d60a422dcf911a38bce8e63d
JiyinShao <161404316+JiyinShao@users.noreply.github.com>
2025-07-26 20:32:43 +0930

    Create llm_interface.py

---
+++ llm_interface.py
@@ -0,0 +1,10 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("t5-base")
+model = T5ForConditionalGeneration.from_pretrained("t5-base")
+
+def query_llm(prompt):
+    inputs = tokenizer(prompt, return_tensors="pt", truncation=True, max_length=512)
+    output_ids = model.generate(inputs["input_ids"], max_length=64, num_beams=4, early_stopping=True)
+    output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return output
